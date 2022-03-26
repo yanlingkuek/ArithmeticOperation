706950a3b6ef92f21d614c78937330335be9bae7
yanlingkuek <61675952+yanlingkuek@users.noreply.github.com>
2022-03-26 16:02:50 -0400

    Comment out ArithmeticOperationsTest.testSub

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -17,6 +17,7 @@ public class ArithmeticOperationsTest {
 		assertEquals(expected, actual);
 
 	}
+    /*
     @Test
     public void testSub()
     {
@@ -27,5 +28,6 @@ public class ArithmeticOperationsTest {
         assertEquals(expected, actual);
 
     }
+    */
 
 }
